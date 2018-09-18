6d1fa230980af7902ed24f6409eb371373e1f44c
hao.xie <hao.xie@hzztai.com>
2018-09-18 16:18:56 +0800

    string2qrcode

--- src/components/string2qrcode.vue
+++ src/components/string2qrcode.vue
@@ -1,14 +1,33 @@
 <template>
   <div class="qrcode-page" style="text-align:left;padding:20px;">
-    <div style="padding:50px 0;"> 
-      <p>请输入任意字符串（网址，名称等）: </p>
-      <el-input v-model="stringValue" type="textarea" style="width:300px;margin-top:25px;"></el-input> 
-      <el-button type="primary" size="small" @click="sendString">发送</el-button>
+    <h3 style="text-align: center">二维码自动生成工具</h3>
+    <div class="container">
+      <div class="left-content">
+        <p>请输入任意字符串（网址，名称等）: </p>
+        <el-input v-model="stringValue" type="textarea" style="width:300px;"></el-input>
+        <p>请选择二维码颜色：（不选默认黑色）</p>
+        <el-color-picker
+          v-model="darkColor"
+          color-format="hex">
+        </el-color-picker>
+        <p>请选择背景颜色：（不选默认白色）</p>
+        <el-color-picker
+          v-model="lightColor"
+          color-format="hex">
+        </el-color-picker>
+        <p>请输入二维码大小：(像素)</p>
+        <el-input-number controls-position="right" v-model="codeSize" size="small" :min="1" :max="1000" :step="16" style="width: 250px;"></el-input-number>
+        <br>
+        <el-button type="primary" size="small" @click="sendString" style="margin-top: 20px">生成</el-button>
+      </div>
+      <div class="right-content">
+        <p> QRcode: </p>
+        <img :src="QRcodeUrl" style="margin:5px 0 0 55px;" />
+        <p> imgUrl: </p>
+        <el-input id="imgUrl" v-model="imgUrl" type="textarea" style="width:320px;" resize="both" :autosize="{minRows: 2,maxRows: 4}"></el-input>
+        <el-button @click="copy" size="mini">copy</el-button>
+      </div>
     </div>
-    <p> QRcode: </p>
-    <img :src="QRcodeUrl" style="margin:5px 0 0 55px;" />
-    <p> imgUrl: </p>
-    <p style="margin-top:15px; width:90%;overflow: hidden; word-break:normal; white-space:pre-warp; word-wrap:break-word;"> {{imgUrl}}</p>
   </div>
 </template>
 
@@ -16,60 +35,54 @@
   export default {
     data() {
       return {
-       stringValue: '',
-       QRcodeUrl: '',
-       imgUrl: '',
+        stringValue: '',
+        QRcodeUrl: '',
+        imgUrl: '',
+        darkColor: '#000000',
+        lightColor: '#ffffff',
+        codeSize: 116,
       }
     },
     created() {
-   
+
     },
-    methods:{
-     sendString() {
-       this.$http.post('https://api.xiehao.xin/string2qrcode',{stringVal:this.stringValue}).then((res) =>{
-        this.QRcodeUrl = res.body.imgUrl;
-        this.imgUrl = res.body.imgUrl;
-       })
-     }
+    methods: {
+      sendString() {
+        //let url = 'https://api.xiehao.xin/string2qrcode';
+        let url = 'http://localhost:8989/string2qrcode';
+        let params = {
+          stringVal: this.stringValue,
+          codeSize: this.codeSize,
+          darkColor: this.darkColor?`${this.darkColor}ff`:undefined,
+          lightColor: this.lightColor?`${this.lightColor}ff`:undefined,
+        }
+        this.$http.post(url, params).then((res) => {
+          this.QRcodeUrl = res.body.imgUrl;
+          this.imgUrl = res.body.imgUrl;
+        })
+      },
+      copy() {
+        const imgUrl = document.querySelector('#imgUrl');
+        imgUrl.select();
+        if (document.execCommand('copy')) {
+          document.execCommand('copy');
+          this.$message.success('复制成功');
+        }
+      }
     }
   }
 </script>
 
 <style scoped>
-  .ooxx-wrapper{
-    column-count: 6;
-    column-gap: 0;
-  }
-  .item{
-    break-inside: avoid;
-    padding: 10px;
-  }
-  .item-content{
+  .container{
+    margin-top: 50px;
     display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: auto;
-    cursor: pointer;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    -webkit-flex-wrap: wrap;
+    -ms-flex-wrap: wrap
   }
-  @media (max-width: 1367px) {
-    .ooxx-wrapper{
-      column-count: 4;
-    }
-  }
-  @media (max-width: 1025px) {
-    .ooxx-wrapper{
-      column-count: 3;
-    }
-  }
-  @media (max-width: 720px) {
-    .ooxx-wrapper{
-      column-count: 2;
-    }
-  }
-  @media (max-width: 560px) {
-    .ooxx-wrapper{
-      column-count: 1;
-    }
+  p{
+    margin: 20px 0;
   }
 </style>
